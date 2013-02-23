bf372014804392849710e4039c352af0fc91a7ac
milimetric <dandreescu@wikimedia.org>
2013-02-23 16:53:01 -0500

    ssh config hack to try to get travis to push

--- .compile-script.sh
+++ .compile-script.sh
@@ -4,6 +4,11 @@ git config user.email "travis@justicepartyofpa.org"
 git config user.name "Travis Automation"
 git config remote.origin.url "git@github.com:JusticeParty/founding-documents.git"
 
+# hack the ssh config file so we can push
+cp ~/.ssh/config ~/.ssh/config.bak
+echo "Host github.com" > ~/.ssh/config
+echo "    StrictHostKeyChecking no" >> ~/.ssh/config
+
 ./.compile-documents.py
 git add .
 git commit -a -m "updating compiled documents"
@@ -16,3 +21,6 @@ git commit -a -m "updating compiled documents"
 git push
 
 git checkout master
+
+# unhack the ssh config file
+cp ~/.ssh/config.bak ~/.ssh/config
