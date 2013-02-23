1a9bc215544f37f8c2aacec14c568afd63d6467f
milimetric <dandreescu@wikimedia.org>
2013-02-23 15:51:04 -0500

    accidentally copied here through script

--- .compile-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./.compile-documents.py
-git checkout gh-pages
-git checkout master -- _posts
-git commit -a -m "updating compiled documents"
-git checkout master
-git commit -a -m "updating compiled documents"
-git push
