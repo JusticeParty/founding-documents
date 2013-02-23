a6d27ff88f9b3a2edd2f5bd0db9fc005030ef2b9
JusticeParty <eggshelly@gmail.com>
2013-02-23 16:29:36 -0500

    fixing git config problems

--- .compile-script.sh
+++ .compile-script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git config user.email "travis@justicepartyofpa.org"
+git config user.name "Travis Automation"
+git config remote.origin.url "git@github.com:JusticeParty/founding-documents.git"
+
 ./.compile-documents.py
 git add .
 git commit -a -m "updating compiled documents"
